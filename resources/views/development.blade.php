47d3b19cf5013e20f2c19f5dba8db9ac23faca6a
unknown <fm.felipe@gmail.com>
2022-06-08 21:04:57 -0400

    ajustes dependências

---
+++ resources/views/development.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('vendor/adminlte/css/adminlte.css')}}">
+    @livewireStyles
+
+</head>
+<body>
+
+
+    
+    
+
+    <script src="{{asset('js/app.js')}}"></script>
+    <script src="{{asset('vendor/adminlte/js/adminlte.js')}}"></script>
+    @livewireScripts
+
+</body>
+</html>
